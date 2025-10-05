367a18834ffdcafcca9d0bd5603efbc9416a9204
Jakub Zawadzki <jzawadzki19@gmail.com>
2025-10-05 02:47:58 +0200

    fix button highlight

--- src/app/wrapper-page/wrapper-page.component.ts
+++ src/app/wrapper-page/wrapper-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'mv-wrapper-page',
@@ -8,6 +8,13 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './wrapper-page.component.html',
   styleUrl: './wrapper-page.component.scss'
 })
-export class WrapperPageComponent {}
+export class WrapperPageComponent {
+  constructor(private readonly router: Router) {}
+
+  protected isCheckupActive(): boolean {
+    const url = this.router.url;
+    return url.startsWith('/app/checkup') || url.startsWith('/app/daily');
+  }
+}
 
 
